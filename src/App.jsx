5bf3e9610a1c8cd6b9d74055a2483f3b0829461b
Naassom Pedro <naassom7@gmail.com>
2022-06-08 21:39:23 -0300

    feat: create navbar-items with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/Header";
+import Navbar from './components/Navbar/Navbar';
 
 
 
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 function App() {
   return (
 
-      <Header />
+      <Navbar />
 
 
   )
---
+++ src/components/Navbar/MenuItems.jsx
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'navlinks'
+    },
+    {
+        title: 'Currículo',
+        url: '#',
+        cName: 'navlinks'
+    },
+    {
+        title: 'Apresentação',
+        url: '#',
+        cName: 'navlinks'
+    },
+    {
+        title: 'Portifólio',
+        url: '#',
+        cName: 'navlinks'
+    },
+    {
+        title: 'Contato',
+        url: '#',
+        cName: 'navlinks'
+    },
+]
